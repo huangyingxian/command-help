edacf9af7dea642382ad7f02416f91b65c7a5c90
huangyingxian <huangyingxian@dm-ai.cn>
2021-02-24 20:14:48 +0800

    更新README文档

--- README.md
+++ README.md
@@ -5,6 +5,48 @@ Nodejs command help module
 ## 项目介绍
 类似一些软件提供的命令行帮助文档，如 node -h | --help. python --version 等。本项目提供node 执行脚本时的一个命令帮助工具
 
+## 特性分析
+1. dest：要添加到由parse_options（）返回的对象的属性的名称
+如果使用者未传入 dest ,cli.option(flags, addition = {}) 时，dest 默认取 flags 数组最后一个命令驼峰后的结果
+2. cli.option(flags, addition = {}) 之后的结构体
+```js
+// 1.基本操作
+{
+    "action": "help",
+    "dest": "help",
+    "help": "output usage information",
+    "flags": [
+        "-h",
+        "--help"
+    ],
+    "positional": false
+}
+// 2.常用单个参数
+{
+    "metavar": "address",
+    "help": "specify server host",
+    "dest": "address",
+    "flags": [
+        "-a",
+        "--address"
+    ],
+    "positional": false
+}
+// 2. 使用了 nargs 参数
+{
+    "metavar": "source",
+    "nargs": "+",
+    "help": "set priority of sources",
+    "dest": "matchOrder",
+    "flags": [
+        "-o",
+        "--match-order"
+    ],
+    "positional": false
+}
+```
+
+
 
 ## 特性
 
--- test/test.js
+++ test/test.js
@@ -5,16 +5,16 @@ const commandHelp = require('../index');
 
 
 commandHelp.program({name: package.name.replace(/@.+\//, ''), version: package.version})
+.option(['-h', '--help'], {action: 'help'})
 .option(['-v', '--version'], {action: 'version'})
 .option(['-p', '--port'], {metavar: 'port', help: 'specify server port'})
 .option(['-a', '--address'], {metavar: 'address', help: 'specify server host'})
 .option(['-u', '--proxy-url'], {metavar: 'url', help: 'request through upstream proxy'})
 .option(['-f', '--force-host'], {metavar: 'host', help: 'force the netease server ip'})
-.option(['-o', '--match-order'], {metavar: 'source', nargs: '+', help: 'set priority of sources'})
 .option(['-t', '--token'], {metavar: 'token', help: 'set up proxy authentication'})
 .option(['-e', '--endpoint'], {metavar: 'url', help: 'replace virtual endpoint with public host'})
 .option(['-s', '--strict'], {action: 'store_true', help: 'enable proxy limitation'})
-.option(['-h', '--help'], {action: 'help'})
+.option(['-o', '--match-order'], {metavar: 'source', nargs: '+', help: 'set priority of sources'})
 .parse(process.argv);
 
 
